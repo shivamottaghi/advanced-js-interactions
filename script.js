88a60232a007c06d4c13a798983f26a63199f91c
shivamtg <shivamtg1376@gmail.com>
2022-05-30 09:30:29 +0200

    add files

---
+++ script.js
@@ -0,0 +1,8 @@
+Array.from(document.querySelectorAll(".letter")).forEach(el => {
+   el.innerText = randomLetter();
+});
+
+function randomLetter(){
+    const alphabet = "abcdefghijklmnopqrstuvwxyz"
+    return alphabet[Math.floor(Math.random() * alphabet.length)]
+}

8a35073171d54df64309fdccf8fecd9dd45a00fe
shivamtg <shivamtg1376@gmail.com>
2022-05-30 15:20:11 +0200

    add Images to the collage

--- script.js
+++ script.js
@@ -41,8 +41,76 @@ const createCarousel = () => {
 }
 createCarousel();
 /*----------collage----------*/
+let aotArr= [
+    {
+        'name': 'Eren Yeager',
+        'Titan' : 'Attack , Founder , War Hammer',
+        'Ability': 'The Attack Titan Receive memories from both past and future inheritors of the Titan.(You can search the rest yourself I don\'t have time! &#128527;)',
+        'imgUrl': 'images/AOT/Eren.jpg',
+        'clicked': false
+    },
+    {
+        'name': 'Armin Arlert',
+        'Titan' : 'Colossus',
+        'Ability': 'The Colossus Titan is enormous and can emit immense amounts of steam from its body like a blast furnace.',
+        'imgUrl': 'images/AOT/Armin.jpg',
+        'clicked': false
+    },
+    {
+        'name': 'Mikasa Ackermann',
+        'Titan' : 'None',
+        'Ability': 'Physical strength,she can easily take down Titans, to be specific she\'s awesome &#128528;',
+        'imgUrl': 'images/AOT/Mikasa.png',
+        'clicked': false
+    },
+    {
+        'name': 'Zeke Yeager',
+        'Titan' : 'Beast',
+        'Ability': 'The Beast Titan could take on the physical characteristics of different animals that vary between its inheritors. It\'s very creepy believe me',
+        'imgUrl': 'images/AOT/Zeke Yeager.png',
+        'clicked': false
+    },
+    {
+        'name': 'Annie Leonhart',
+        'Titan' : 'Female Titan',
+        'Ability': 'The Female Titan could inherit abilities much more easily than any of the other Titans. It could also harden areas of its skin for extra offensive and defensive capabilities.',
+        'imgUrl': 'images/AOT/annie leonhart.jpg',
+        'clicked': false
+    },
+    {
+        'name': 'Reiner Braun',
+        'Titan' : 'Armor',
+        'Ability': 'The Armored Titan was covered by hardened, armor-like skin, except in key areas necessary for movement.',
+        'imgUrl': 'images/AOT/Reiner_.jpg',
+        'clicked': false
+    },
+    {
+        'name': 'Pieck Finger',
+        'Titan' : 'Cart',
+        'Ability': 'The Cart Titan was quadrupedal and had great endurance that allowed it to stay transformed for long periods of time, as well as transform in rapid succession without needing to rest, hundreds of times per day even. ',
+        'imgUrl': 'images/AOT/pieck.jpg',
+        'clicked': false
+    },
+    {
+        'name': 'Falco Grice',
+        'Titan' : 'Jaw',
+        'Ability': 'The Jaw Titan was the swiftest of the Nine Titans and had sharp, hardened teeth and claws capable of damaging or breaking almost anything.',
+        'imgUrl': 'images/AOT/Falco.jpg',
+        'clicked': false
+    }
+    ];
 const createImgTag = (theClass) => {
     let img = document.createElement('img');
     img.setAttribute('class', theClass);
     return img;
 }
+const addImageCollage = () =>{
+    let imgArr = document.querySelectorAll('.image');
+    for (let i = 0; i< aotArr.length; i++){
+        let img = createImgTag('collageImg');
+        img.src = aotArr[i].imgUrl;
+        img.height = 250;
+        imgArr[i].appendChild(img);
+    }
+}
+addImageCollage();

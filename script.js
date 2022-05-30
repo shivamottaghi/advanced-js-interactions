ecf476a6058f463534f02e449695bb33e4d76752
shivamtg <shivamtg1376@gmail.com>
2022-05-30 10:15:30 +0200

    dark mode- change bgcolor and text color

--- script.js
+++ script.js
@@ -6,3 +6,17 @@ function randomLetter(){
     const alphabet = "abcdefghijklmnopqrstuvwxyz"
     return alphabet[Math.floor(Math.random() * alphabet.length)]
 }
+/*-----------dark mode-------------*/
+let darkMode = false;
+document.querySelector('.mode').addEventListener('click', ()=>{
+    darkMode = !darkMode;
+    changeMode(darkMode);
+})
+
+const changeMode = (darkMode) => {
+    if (darkMode){
+        document.body.classList.add('dark-mode');
+    }else {
+        document.body.classList.remove('dark-mode');
+    }
+}

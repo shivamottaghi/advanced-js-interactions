144df83f16095bea759a684e1b25ec09bb657cc8
shivamtg <shivamtg1376@gmail.com>
2022-05-30 11:17:54 +0200

    add carousel-works with interval

--- script.js
+++ script.js
@@ -20,3 +20,34 @@ const changeMode = (darkMode) => {
         document.body.classList.remove('dark-mode');
     }
 }
+/*----------carousel-----------*/
+let imgUrl = ['images/1.png','images/2.jpg','images/3.png'];
+//let carouselImgAdded = false;
+const carouselInterval = (i)=>{
+    setInterval(()=>{
+        if (i == imgUrl.length){
+            i = 0;
+        }
+        let myImg = document.querySelector('.carouselImg');
+        myImg.src = imgUrl[i];
+        i++;
+
+    },2000)
+}
+const createImgTag = (theClass) => {
+    let img = document.createElement('img');
+    img.setAttribute('class', theClass);
+    return img;
+}
+const createCarousel = () => {
+    let parent = document.querySelector('.carouselContainer')
+    let img = createImgTag('carouselImg');
+    //img.height = 400;
+    img.setAttribute('width', '100%');
+    img.src = imgUrl[imgUrl.length-1];
+    parent.appendChild(img);
+    let i = 0;
+    carouselInterval(i);
+
+}
+createCarousel();
